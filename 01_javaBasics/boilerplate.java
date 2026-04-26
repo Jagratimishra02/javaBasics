73296891ee15561284886a228029a88c4a5ff263
Jagratimishra02 <nishimisra002@gmail.com>
2026-04-26 05:01:21 +0000

    updated

--- 01_javaBasics/boilerplate.java
+++ 01_javaBasics/boilerplate.java
@@ -1,5 +1,6 @@
 public class boilerplate {
     public static void main(String args[]) {
-        
+        System.out.println("1,2,3,4,5");
     }
 }
+// boilerplate code
---
+++ 01_javaBasics/datatypes.java
@@ -0,0 +1,34 @@
+
+// datatypes in java
+   // 1 . primitive                                     // 2. non primitive
+   //      # byte (1 byte)[-128 to 127]-256 numbers         # string
+   //      # short (2 bytes )                               # Array
+   //      # char  (2 bytes)                                # class
+   //      # boolean (1 byte )                              # object
+   //      # int     (4 bytes)                              #  interface
+   //      # long    (8 bytes)
+   //      # float   (4 bytes)
+   //      # double  (8 bytes)
+   //  __________________________________________
+
+
+public class datatypes {
+    public static void main(String[] args) {
+      byte b = 8;
+      System.out.println(b);
+      char ch = 'a';            // char should use (' ') not (" ")
+      System.out.println(ch);
+      boolean var = true ;
+      System.out.println(var);
+      float price = 10.5f ;
+      System.out.println(price);
+      int number = 25;
+      System.out.println(number);
+      long rupees = 100000L ;
+      System.out.println(rupees);
+      short n = 240;
+      System.out.println(n);
+    
+      // bigger num then float then use double for decimals
+    }
+}
