55e12e81cfcfcf090240250921ebc36cf909169a
Jagratimishra02 <nishimisra002@gmail.com>
2026-04-27 17:37:40 +0000

    product

---
+++ 01_javaBasics/Product.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Product {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = a*b;
+        System.out.println(product);
+
+        // area of circle
+        float radius = sc.nextFloat();
+        float area = 3.14F * radius*radius ;
+        System.out.println(area);
+    }
+}
