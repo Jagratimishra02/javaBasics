d1a7dc01fdcbd43fadda45f77637af105c394f84
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-01 12:34:19 +0000

    urnary operator done

---
+++ 02_Operators/UrnaryOperators.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class UrnaryOperators {
+ public static void main(String[] args) {
+       int a = 10;
+       int b = ++a;               // pre increment 
+       System.out.println(a);
+       System.out.println(b);
+
+       int x = 5;
+       int c = x++;               // post increment
+       System.out.println(x);     
+        System.out.println(c);
+
+       int y = 7;
+       int d = --y;                // pre decrement
+       System.out.println(y);  
+       System.out.println(d);
+       
+       int z = 9;
+       int e = z-- ;               // post drecrement
+       System.out.println(z);
+       System.out.println(e);
+    }
+}
