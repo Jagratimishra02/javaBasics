4d013cafb5447a3db965fa2348c84a2ce54abf13
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-02 02:46:24 +0000

    question 1 done

---
+++ 02_Operators/OperatorsQuestions.java
@@ -0,0 +1,18 @@
+// question1 : - what will be output of 
+
+public class OperatorsQuestions {
+ public static void main(String[] args) {
+    int x = 2 , y =5 ;
+    int exp1 = (x*y/x);
+    int exp2 = (x*(y/x));
+    System.out.print(exp1 + " , ");
+    System.out.println(exp2);
+ }
+}
+// Ans 1 = 5,4
+
+
+
+
+
+// Question 2 : - 
