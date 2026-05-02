0f4e4cce36518583050a49fbf04f9207f0b711d2
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-02 14:44:05 +0000

    operatorsQuestion3 done

--- 02_Operators/OperatorsQuestions.java
+++ 02_Operators/OperatorsQuestions.java
@@ -1,14 +1,14 @@
 // question1 : - what will be output of 
 
-public class OperatorsQuestions {
- public static void main(String[] args) {
-    int x = 2 , y =5 ;
-    int exp1 = (x*y/x);
-    int exp2 = (x*(y/x));
-    System.out.print(exp1 + " , ");
-    System.out.println(exp2);
- }
-}
+// public class OperatorsQuestions {
+//  public static void main(String[] args) {
+//     int x = 2 , y =5 ;
+//     int exp1 = (x*y/x);
+//     int exp2 = (x*(y/x));
+//     System.out.print(exp1 + " , ");
+//     System.out.println(exp2);
+//  }
+// }
 // Ans 1 = 5,4
 
 
@@ -16,3 +16,34 @@ public class OperatorsQuestions {
 
 
 // Question 2 : - 
+
+// public class OperatorsQuestions {
+
+//    public static void main(String[] args) {
+//         int x = 200, y = 50, z = 100;
+// if(x > y && y > z){
+// System.out.println("Hello");
+// }
+// if(z > y && z < x){
+// System.out.println("Java");
+// }
+// if((y+200) < x && (y+150) < z){
+// System.out.println("Hello Java");
+// }
+//    }
+// }
+
+
+// Question 3 :-
+
+// public class OperatorsQuestions {
+
+//    public static void main(String[] args) {
+//       int x, y, z;
+//       x = y = z = 2;
+//       x += y;
+//       y -= z;
+//       z /= (x + y);
+//      System.out.println(x + " " + y + " " + z);
+//    }
+// }
