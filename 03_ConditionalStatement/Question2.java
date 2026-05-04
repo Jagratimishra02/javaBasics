a46c8c2a7449648b7355ca6cdbda396981d65ba3
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-04 16:46:55 +0000

    odd even question completed

---
+++ 03_ConditionalStatement/Question2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Question2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Num = sc.nextInt();
+        if (Num % 2 == 0) {
+            System.out.println("number is even");
+        } else{
+            System.out.println("number is odd");
+        }
+    }
+}
