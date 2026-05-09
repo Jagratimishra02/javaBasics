1d59f8b6f9ce8bd26d4f70fcbab01704aced4058
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-09 15:24:56 +0000

    Question5 done

---
+++ 03_ConditionalStatement/Question5.java
@@ -0,0 +1,8 @@
+// print reportcard of students
+public class Question5 {
+    public static void main(String[] args) {
+        int marks = 48;
+        String Reportcard = (marks>= 33)?"pass":"fail";
+        System.out.println(Reportcard);
+    }
+}
