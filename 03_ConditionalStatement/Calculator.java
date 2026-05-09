20e20b8537177c2fdc7b91e8120e671cba0a61a7
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-09 16:30:02 +0000

    Calculator completed

--- 03_ConditionalStatement/Calculator.java
+++ 03_ConditionalStatement/Calculator.java
@@ -1,4 +1,4 @@
-import java.util.*;*;
+import java.util.*;
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,7 +8,21 @@ public class Calculator {
         int b = sc.nextInt();
         System.out.println("Enter Operator :");
         char operator = sc.next().charAt(0);
-        
+
+        switch (operator) {
+            case '+' : System.out.println(a + b);
+                   break;
+            case '-' : System.out.println(a - b); 
+                   break;
+            case '*' : System.out.println(a * b); 
+                   break;
+            case '/' : System.out.println(a / b); 
+                   break;
+            case '%' : System.out.println(a % b); 
+                   break;
+            default : System.out.println("invalid operator");
+                 
+        }
 
     }
 }
