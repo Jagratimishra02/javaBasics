3dbeabe66a3a917bc3e36fd2778302f7089cbceb
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-09 16:14:51 +0000

    calc on the way

--- 03_ConditionalStatement/Calculator.java
+++ 03_ConditionalStatement/Calculator.java
@@ -1,6 +1,10 @@
 import java.util.*;*;
 public class Calculator {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number a :");
+        int a = sc.nextInt();
+        System.out.println("Enter a number b :");
+        int b = sc.nextInt();
     }
 }
