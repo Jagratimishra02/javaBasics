bfde835521d8ba919ae9456644790c30f192782c
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-09 16:02:51 +0000

    Switch done

---
+++ 03_ConditionalStatement/Switch.java
@@ -0,0 +1,20 @@
+public class Switch {
+    public static void main(String[] args) {
+        char ch = 'a';
+        switch (ch) {
+            case 'b':
+                System.out.println("samosa");
+                break;
+            case 'a':   
+                 System.out.println("burger");  
+                break;
+            case 'c':   
+                 System.out.println("pasta");  
+                break;
+
+            default: System.out.println("wake up");
+                break;
+        }
+
+    }
+}
