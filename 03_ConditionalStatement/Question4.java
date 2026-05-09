3f5c1a81c5ff2441af34422e2323d23dfaa03507
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-09 14:06:15 +0000

    question4 done

---
+++ 03_ConditionalStatement/Question4.java
@@ -0,0 +1,12 @@
+public class Question4 {
+    public static void main(String[] args) {
+        int a = 1 , b = 3 , c = 6 ;
+        if (a > b && a > c) {
+            System.out.println("a is larger");
+        } else if (b > c) {
+          System.out.println("b is larger");  
+        } else {
+            System.out.println("c is larger");
+        }
+    }
+}
