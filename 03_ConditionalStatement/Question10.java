e4ec3411e9eb9458cdd8d8d2705bcd3e38cec956
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-10 13:46:26 +0000

    leap year advanced method

--- 03_ConditionalStatement/Question10.java
+++ 03_ConditionalStatement/Question10.java
@@ -8,15 +8,27 @@ public class Question10 {
         System.out.println("enter year : ");
         int year = sc.nextInt();
         System.out.println(year);
-        if (year % 4 != 0){
+        // if (year % 4 != 0){
+        //     System.out.println(year + "is not a leap year");
+        //    }  else if (year % 100 != 0){
+        //     System.out.println(year + "it is a leap year");
+        //     } else if (year % 400 == 0) {
+        //         System.out.println(year + "is  a leap year");
+        // } else {
+        //     System.out.println(year + "is not a leap year");
+        // }
+
+
+    //_______________________________method 2 (advanced) _________________________________________
+
+         boolean x = (year % 4 == 0);
+         boolean y = (year % 100 != 0);
+         boolean z = (year % 100 == 0) && (year % 400 == 0);
+         if (x && (y || z)) {
+            System.out.println(year + "is a leap year");
+         } else {
             System.out.println(year + "is not a leap year");
-           }  else if (year % 100 != 0){
-            System.out.println(year + "it is a leap year");
-            } else if (year % 400 == 0) {
-                System.out.println(year + "is  a leap year");
-        } else {
-            System.out.println(year + "is not a leap year");
-        }
+         }
 
     }
 }
