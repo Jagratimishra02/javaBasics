9b0aff5b6cd585402d45cde525483faf401c8a55
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-10 02:56:33 +0000

    positive negative question completed

--- 03_ConditionalStatement/Question6.java
+++ 03_ConditionalStatement/Question6.java
@@ -5,7 +5,9 @@ import java.util.*;
 public class Question6 {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int num = sc.nextInt()
+    int num = sc.nextInt();
+    String Answer = (num < 0 )? "negative" : "positive";
+    System.out.println(Answer);
 }
     
 }
