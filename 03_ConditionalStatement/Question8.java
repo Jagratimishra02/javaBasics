8b3e42dd094170ddd6b1752110cdb8cfa3bf484f
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-10 12:47:38 +0000

    Question 8 done

---
+++ 03_ConditionalStatement/Question8.java
@@ -0,0 +1,28 @@
+//Question 3 : Write a Java program to input week number(1-7) and print day of week name
+// using switch case.
+import java.util.*;
+public class Question8 {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int day = sc.nextInt();
+      switch (day) {
+        case 1 : System.out.println("sunday");
+            break;
+        case 2 : System.out.println("monday");
+            break;
+        case 3 : System.out.println("tuesday");
+            break;
+        case 4: System.out.println("wednesday");
+            break;
+        case 5 : System.out.println("thursday");
+            break;
+        case 6 : System.out.println("friday");
+            break;
+        case 7 : System.out.println("saturday");
+            break;
+      
+        default:
+            break;
+      }
+    }
+}
