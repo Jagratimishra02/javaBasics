9073eace5141a8bcc65fdc02745da7ecb903667d
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-10 13:33:52 +0000

    leap year question done

---
+++ 03_ConditionalStatement/Question10.java
@@ -0,0 +1,22 @@
+//Question 5 : Write a Java program that takes a year from the user and print whether that
+// year is a leap year or not.
+
+import java.util.*;
+public class Question10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter year : ");
+        int year = sc.nextInt();
+        System.out.println(year);
+        if (year % 4 != 0){
+            System.out.println(year + "is not a leap year");
+           }  else if (year % 100 != 0){
+            System.out.println(year + "it is a leap year");
+            } else if (year % 400 == 0) {
+                System.out.println(year + "is  a leap year");
+        } else {
+            System.out.println(year + "is not a leap year");
+        }
+
+    }
+}
