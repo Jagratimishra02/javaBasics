c8b2a334ec007a6b83412845f95f6273eb60c9b7
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-10 13:00:01 +0000

    question 9 done

---
+++ 03_ConditionalStatement/Question9.java
@@ -0,0 +1,10 @@
+// Question 4 : What will be the value of x & y in the following
+public class Question9 {
+    public static void main(String[] args) {
+        int a = 63, b = 36 ;
+        boolean x = (a<b)?true:false;
+        System.out.println(x);
+        int y = (a>b)?a:b;
+        System.out.println(y);
+    }
+}
