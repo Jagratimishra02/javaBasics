e6810b61a12432818707e2df344a0da66670f1b7
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-10 02:50:29 +0000

    qusetion 6 started

---
+++ 03_ConditionalStatement/Question6.java
@@ -0,0 +1,11 @@
+//Question 1 : Write a Java program to get a number from the user and print whether it is
+// positive or negative.
+
+import java.util.*;
+public class Question6 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt()
+}
+    
+}
