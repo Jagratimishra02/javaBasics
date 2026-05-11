334b845b0c400be931e75dfeff0e21d4768af086
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-11 05:44:47 +0000

    modified

---
+++ 04_Loops/LoopQuestion1.java
@@ -0,0 +1,12 @@
+// print number 1 to 100
+
+public class LoopQuestion1 {
+    public static void main(String[] args) {
+        int counter = 1;
+        while (counter<=100) {
+            // System.out.println("counter");
+            System.out.print(counter + " ");  // for same line 
+            counter++;
+        }
+    }
+}
