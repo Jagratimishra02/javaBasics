b967924a5776912546ff4858aed10ebc22f3b2b1
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-11 05:23:17 +0000

    modified

--- 04_Loops/Whileloop.java
+++ 04_Loops/Whileloop.java
@@ -1,10 +1,10 @@
 
 public class Whileloop {
     public static void main(String[] args) {
-        int counter = 1;
+        int counter = 0;                        //initialise 
         while (counter<=10) {
             System.out.println(counter + "hello world");
-            counter++;
+            counter++;                           // ++ means increase by 1 
         }
         System.out.println("code printed 10X");
     }
