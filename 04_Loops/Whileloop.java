1c87d56b006b30439acc1302430ff59c48f5408a
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-11 04:48:50 +0000

    whole loop  done

---
+++ 04_Loops/Whileloop.java
@@ -0,0 +1,11 @@
+
+public class Whileloop {
+    public static void main(String[] args) {
+        int counter = 1;
+        while (counter<=10) {
+            System.out.println(counter + "hello world");
+            counter++;
+        }
+        System.out.println("code printed 10X");
+    }
+}
