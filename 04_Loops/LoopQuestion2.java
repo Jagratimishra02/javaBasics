3625ec406c26a4f6c7c2063a06fb02d875b846e5
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-11 06:53:42 +0000

    Question 2 done

--- 04_Loops/LoopQuestion2.java
+++ 04_Loops/LoopQuestion2.java
@@ -1,6 +1,14 @@
+// print number 1 to n , where n is the input taken by the user
 import java.util.*;
 public class LoopQuestion2 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a input");
+        int input = sc.nextInt();
+        int i = 1;
+        while (i<=input) {
+           System.out.println(i); 
+           i++;
+        }
     }
 }
