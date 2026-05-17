22c35d3578d7c63ad03563ac357dde6bb268b80d
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-17 16:12:51 +0000

    question 5 loops done

--- 04_Loops/LoopQuestion5.java
+++ 04_Loops/LoopQuestion5.java
@@ -2,14 +2,23 @@
 
 public class LoopQuestion5 {
     public static void main(String[] args) {
-        int n = 21204;
-        int rev = 0;
-        while (n > 0) {
-            int lastDigit = n % 10 ;
-            rev = (rev*10) + lastDigit ;
-            n = n/10;
+        // int n = 21204;
+        // int rev = 0;
+        // while (n > 0) {
+        //     int lastDigit = n % 10 ;
+        //     rev = (rev*10) + lastDigit ;
+        //     n = n/10;
            
-        } 
-        System.out.print(rev);
+        // } 
+        // System.out.print(rev);
+
+
+           int rev =0;
+           for ( int n = 21204; n > 0 ; n = n/10 ){
+            int lastdigit = n % 10;
+             rev = (rev * 10) + lastdigit;
+
+           }
+            System.out.print(rev);
     }
 }
