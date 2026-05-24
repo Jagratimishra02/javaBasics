baa77b13610768236ea930b6c4464c752f7a18d9
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-24 11:11:30 +0000

    break question done

--- 04_Loops/BreakQuestion.java
+++ 04_Loops/BreakQuestion.java
@@ -1,3 +1,16 @@
+
+import java.util.*;
 public class BreakQuestion {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       do {
+        System.out.println("Enter your number:");
+        int number = sc.nextInt();
+        if (number%10==0) {
+            break;
+        }
+        System.out.println(number);
+       } while (true);
+
+    }
 }
