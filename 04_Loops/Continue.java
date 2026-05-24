61604744892b01ead1fc4836991cdb056972689e
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-24 11:18:13 +0000

    comment added

--- 04_Loops/Continue.java
+++ 04_Loops/Continue.java
@@ -1,3 +1,7 @@
+// continue : - continue is used to skip an iteration 
+// for example:-skip 3 and print from 0 to 5
 public class Continue {
-    
+    public static void main(String[] args) {
+        
+    }
 }
