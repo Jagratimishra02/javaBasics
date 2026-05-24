3e6b399dc817e973cff5eb830890fca3f4daa2b2
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-24 11:29:00 +0000

    continue question done

--- 04_Loops/Continue.java
+++ 04_Loops/Continue.java
@@ -2,6 +2,13 @@
 // for example:-skip 3 and print from 0 to 5
 public class Continue {
     public static void main(String[] args) {
-        
+        for(int i=1;i<=5;i++){
+            {
+              if (i==3) {
+                continue;
+              }
+            }
+            System.out.println(i);
+        }
     }
 }
