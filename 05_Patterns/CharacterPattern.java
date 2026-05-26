9ffd8da8cd17710838308ba21bf5898aec8f1c0f
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-26 11:55:03 +0000

    character pattern completed

--- 05_Patterns/CharacterPattern.java
+++ 05_Patterns/CharacterPattern.java
@@ -1,5 +1,14 @@
+// character pattern
 public class CharacterPattern {
     public static void main(String[] args) {
-        
+        int n =4;
+        char ch ='A';
+        for(int line = 1; line<= n; line++){
+            for(int chars =1; line>= chars;chars++){
+                System.out.print(ch);
+                ch++;                                   // this will update the character
+            }
+            System.out.println();
+        }
     }
 }
