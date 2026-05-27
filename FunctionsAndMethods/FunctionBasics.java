a8a3922f1aaa4020294e82f04d6c7e183e013e57
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-27 13:06:36 +0000

    comment added

--- FunctionsAndMethods/FunctionBasics.java
+++ FunctionsAndMethods/FunctionBasics.java
@@ -1,4 +1,10 @@
 // new function
+// PARAMETERS:-the values which are passed in parathesis of function is known as parameters or formal parameters. 
+// eg:- public static int Calculatesum(int num1 ,int num2)  , num1 & num2 are parameters.
+
+// ARGUMENTS:-the values which are called or passes in a function is called arguments or actual parameters.
+// Eg:- Calculatesum(a,b);      , a & b are arguments passed in calculate sum function.
+
 import java.util.*;
 public class FunctionBasics {
     public static void printHelloWorld(){
@@ -7,7 +13,7 @@ public class FunctionBasics {
         System.out.println("hello world");
     }
     
-    public static int Calculatesum(int num1 ,int num2){              // parameter names can also be int a and int b.
+    public static int Calculatesum(int num1 ,int num2){            // parameter names can also be int a and int b.
         Scanner sc = new Scanner(System.in);
         int sum = num1+num2;
         System.out.println(sum);
