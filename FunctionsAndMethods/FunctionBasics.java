8d19103151c9c2c526cfdba3f16f62f26cf197b9
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-27 12:53:43 +0000

    code completed

--- FunctionsAndMethods/FunctionBasics.java
+++ FunctionsAndMethods/FunctionBasics.java
@@ -1,4 +1,5 @@
 // new function
+import java.util.*;
 public class FunctionBasics {
     public static void printHelloWorld(){
         System.out.println("hello world");
@@ -6,9 +7,18 @@ public class FunctionBasics {
         System.out.println("hello world");
     }
     
-    public static void Calculate
+    public static int Calculatesum(int num1 ,int num2){              // parameter names can also be int a and int b.
+        Scanner sc = new Scanner(System.in);
+        int sum = num1+num2;
+        System.out.println(sum);
+        return sum;
+    }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
     printHelloWorld();
+    Calculatesum(a,b);
    } 
 }
