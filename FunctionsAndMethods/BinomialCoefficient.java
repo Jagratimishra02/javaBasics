851d19eac0d4b98f33f7b2e9d19e6687f3faf4be
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-29 03:36:05 +0000

    binomial coefficient started

--- FunctionsAndMethods/BinomialCoefficient.java
+++ FunctionsAndMethods/BinomialCoefficient.java
@@ -1,4 +1,25 @@
 // binomial coefficient
 public class BinomialCoefficient {
-    
+    public static int factorial(int n){
+        int f = 1;
+        for(int i = 1; i<= n ; i++){
+            f = f*i;
+        }
+        return f ;
+    }
+
+public static int binoCoeff(int n , int r){
+    int fact_n = factorial(n);
+    int fact_r = factorial(r);
+    int fact_nmr = factorial(n-r);                                // factorial n minus r
+
+    int binoCoeff = fact_n/(fact_r*fact_nmr);
+    return binoCoeff;
+}
+
+
+public static void main(String[] args) {
+    //    System.out.println(factorial(3)); 
+       System.out.println("binomial coefficient is : " + binoCoeff(5, 2)); 
+    }
 }
