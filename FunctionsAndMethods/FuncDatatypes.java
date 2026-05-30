a0855f367f606c35bf4ad4d981ec6acc6a4f9021
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-30 10:57:58 +0000

    updated

---
+++ FunctionsAndMethods/FuncDatatypes.java
@@ -0,0 +1,15 @@
+public class FuncDatatypes {
+
+// function to calculate int sum.
+    public static int sum (int a, int b){
+    return a+b;
+       }
+// function to calculate float sum.
+    public static float sum (float a, float b){
+    return a+b;
+       }
+    public static void main(String[] args) {
+        System.out.println(sum(2,6));
+        System.out.println(sum(3.4f,4.6f));
+    }
+}
