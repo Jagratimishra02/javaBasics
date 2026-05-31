63d9927d6fc62eff2a1da88b5220c3ef906f43ff
Jagratimishra02 <nishimisra002@gmail.com>
2026-05-31 04:10:03 +0000

    modified

--- FunctionsAndMethods/PrimeNumCheck.java
+++ FunctionsAndMethods/PrimeNumCheck.java
@@ -2,13 +2,16 @@ public class PrimeNumCheck {
     public static boolean isPrime(int n){
         Boolean isPrime = true;
         for(int i = 2 ; i <= n-1; i++){
-            if (i % n == 0) {              //Completely divide
+            if (n % i == 0) {              //Completely divide
               isPrime = false;
+              break;
             }
-           
         }
+        return isPrime;
     }
     public static void main(String[] args) {
-        
+       System.out.println(isPrime(2));
+       System.out.println(isPrime(2));
+       System.out.println(isPrime(2));
     }
 }
