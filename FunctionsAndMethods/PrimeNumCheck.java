c7dd2ecbff19f00cb8ac1e488d82da45dad3d108
Jagratimishra02 <nishimisra002@gmail.com>
2026-06-02 03:53:25 +0000

    prime number optimised way

--- FunctionsAndMethods/PrimeNumCheck.java
+++ FunctionsAndMethods/PrimeNumCheck.java
@@ -1,14 +1,26 @@
 public class PrimeNumCheck {
+    // public static boolean isPrime(int n){
+    //     Boolean isPrime = true;
+    //     for(int i = 2 ; i <= n-1; i++){
+    //         if (n % i == 0) {              //Completely divide
+    //           isPrime = false;
+    //           break;
+    //         }
+    //     }
+    //     return isPrime;
+    // }
+
+    // METHOD 2 :- optimised way 
     public static boolean isPrime(int n){
         Boolean isPrime = true;
-        for(int i = 2 ; i <= n-1; i++){
-            if (n % i == 0) {              //Completely divide
-              isPrime = false;
-              break;
+        for (int i = 2 ; i <= math.sqrt(n);i++){
+            if ( n % i == 0){
+                isPrime = false;
             }
         }
-        return isPrime;
-    }
+   }
+
+
     public static void main(String[] args) {
        System.out.println(isPrime(2));
        System.out.println(isPrime(8));
