9cc450d20b28f61297124dbd6319463f34d9ed93
Jagratimishra02 <nishimisra002@gmail.com>
2026-06-04 04:26:01 +0000

    prime number updated

--- FunctionsAndMethods/PrintPrimeRange.java
+++ FunctionsAndMethods/PrintPrimeRange.java
@@ -15,13 +15,13 @@ public class PrintPrimeRange {
 
     public static void PrimesInRange(int n){
         for (int i = 2 ; i <= n ; i++){
-            if (isPrime(i)) {
+            if (isPrime(i)) {  // if true 
                 System.out.print(i +" ");
             }
          } 
             System.out.println();
     }
     public static void main(String[] args) {
-        PrimesInRange(10);
+        PrimesInRange(100); // 2 to 20 
     }
 }
