c5e01b2598ba1587e79ec3114967441c5f8124e5
Jagratimishra02 <nishimisra002@gmail.com>
2026-06-09 06:55:33 +0000

    binary conversion

--- FunctionsAndMethods/DecToBin.java
+++ FunctionsAndMethods/DecToBin.java
@@ -1,6 +1,10 @@
 // Dec to binary conversion 
 import java.util.*;
 public class DecToBin {
+    public static void DecToBin(int BinNum){
+        int Pownum = 0;
+        int Decnum = 0;
+    }
     public static void main(String[] args) {
         
     }
