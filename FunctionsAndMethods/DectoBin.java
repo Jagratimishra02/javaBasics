fb2196b87f325e5da6559d4da0738268c316a469
Jagratimishra02 <nishimisra002@gmail.com>
2026-06-10 07:17:24 +0000

    decimal to binary conversion done

---
+++ FunctionsAndMethods/DectoBin.java
@@ -0,0 +1,17 @@
+public class DectoBin {
+    public static void DectoBin(int n){
+        int myNum = n ;
+        int Pow = 0 ;
+        int BinNum = 0;
+        while (n>0) {
+            int rem = n % 2 ;
+            BinNum = BinNum + (rem * (int)Math.pow(10, Pow));
+            Pow++;
+            n = n/2;
+        }
+        System.out.println("binNum of " +  myNum + "=" + BinNum);
+    }
+    public static void main(String[] args) {
+        DectoBin(15);
+    }
+}
