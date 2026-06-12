e7a5086b991565517a2d3360a4c46e8cddea722d
Jagratimishra02 <nishimisra002@gmail.com>
2026-06-12 10:52:11 +0000

    average question completed

--- FunctionsAndMethods/FunctionsQuestions.java
+++ FunctionsAndMethods/FunctionsQuestions.java
@@ -1,3 +1,18 @@
+// Question 1 : Write a Java method to compute the average of three numbers..
+import java.util.*;
 public class FunctionsQuestions {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your first number: ");  
+        double x = sc.nextDouble();
+        System.out.println("Enter your second number: ");
+        double y = sc.nextDouble();
+        System.out.println("Enter your third number: ");
+        double z  =sc.nextDouble();
+        System.out.println("The average of three numbers : " + averageNums(x, y, z)); 
+        // System.out.print("The average value is " + averageNums(x, y, z)+"\n" );
+    }
+    public static Double averageNums(double x, double y, double z){
+        return (( x + y + z )/3);
+    }
 }
