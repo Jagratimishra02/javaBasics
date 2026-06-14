06490d9707654ce23948638441b58fed1c1be25b
Jagratimishra02 <nishimisra002@gmail.com>
2026-06-14 15:32:20 +0000

    inverted and rotated pyramid completed

--- Patterns2/InvertedNdRotatedHalfpyramid.java
+++ Patterns2/InvertedNdRotatedHalfpyramid.java
@@ -2,16 +2,19 @@
 
 public class InvertedNdRotatedHalfpyramid {
     public static void main(String[] args) {
-        
+        inverted_rotated_half_pyramid(4);
     }
     public static void inverted_rotated_half_pyramid(int n){
         for (int i = 1 ; i <= n ; i++ ){
             // loop for spaces
             for (int j = 1 ;j <= n-i; j++){
-                System.out.println(" ");
+                System.out.print(" ");
             }
             // loops for stars 
-            
+            for(int j = 1; j <= i;j++){
+            System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
