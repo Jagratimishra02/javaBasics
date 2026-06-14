041c38aef92f204c05d6b22d50f064cefd926232
Jagratimishra02 <nishimisra002@gmail.com>
2026-06-14 05:41:28 +0000

    hollow rectangle

--- Patterns2.java/HollowRec.java
+++ Patterns2.java/HollowRec.java
@@ -1,3 +1,9 @@
+import java.util.*;
 public class HollowRec {
-    
+    public static void main(String[] args) {
+    }
+    public static void main (int row , int column){
+        // int - columns 
+        for(int j =1 ; )
+    }
 }
