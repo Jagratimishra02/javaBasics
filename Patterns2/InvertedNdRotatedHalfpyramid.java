6e0628074c87b3cd0a2c971a2014fddfe01aaff3
Jagratimishra02 <nishimisra002@gmail.com>
2026-06-14 14:50:45 +0000

    pyramid question started

---
+++ Patterns2/InvertedNdRotatedHalfpyramid.java
@@ -0,0 +1,17 @@
+// inverted and rotated half pyramid
+
+public class InvertedNdRotatedHalfpyramid {
+    public static void main(String[] args) {
+        
+    }
+    public static void inverted_rotated_half_pyramid(int n){
+        for (int i = 1 ; i <= n ; i++ ){
+            // loop for spaces
+            for (int j = 1 ;j <= n-i; j++){
+                System.out.println(" ");
+            }
+            // loops for stars 
+            
+        }
+    }
+}
