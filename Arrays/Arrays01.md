3c65fb102ce5f4fc4ea8ad20fd12cb1368c7304b
Jagratimishra02 <nishimisra002@gmail.com>
2026-06-17 07:39:56 +0000

    array creation completed

--- Arrays/Arrays01.md
+++ Arrays/Arrays01.md
@@ -3,3 +3,8 @@ list of elements of same type placed at contiguous memory location.
 
 eg: -      1 , 2 , 3, 4 , 5 - int array
            'a' 'b' 'c' 'd' 'e' - char array
+
+
+# creating an array -
+  dataType ArrayName[] = new datatype[size];
+  Eg:- int marks[] = new int[50];
---
+++ Arrays/Arrays02.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class Arrays02 {
+    public static void main(String[] args) {
+        // create of array - type 1
+        int marks[] = new int[50];
+
+        // or this type in this we directly give element of array instead of size
+        int numbers[]= {1,2,3};    // 3 - array size
+        int moreNumbers[] ={4,5,6}; // 3 array size
+        String fruits[] = {"apple" , "mango" , "banana"}; // 3 - array size
+    }
+}
