e66bebe93b4fd718dcc4a3d3000ad2d236b33df2
Jagratimishra02 <nishimisra002@gmail.com>
2026-06-21 05:05:53 +0000

    binary search completed

---
+++ A_Arrays/BinarySearch.java
@@ -0,0 +1,24 @@
+public class BinarySearch {
+    public static int SearchKey(int num[], int key){
+        int start = 0 , end = num.length-1;
+        while (start <= end) {
+            int mid = (start+end)/2 ;
+            
+            // compare
+            if(num[mid] == key){
+                return mid;
+            }
+            if(num[mid] > key){
+                end = mid - 1;
+            }else {       //  right
+                start = mid + 1 ;
+            }
+        }
+        return -1 ;
+    }
+    public static void main(String[] args) {
+        int num[] = {2,4,6,8,10,12,14};
+        int key = 10;
+        System.out.println("index for key is :" + SearchKey(num, key));
+    }
+}
