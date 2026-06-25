7c5a04130c6188f97597bf5040ca0a5b5715cafd
Jagratimishra02 <nishimisra002@gmail.com>
2026-06-25 09:27:24 +0000

    sub arrays

--- A_Arrays/SubArr.java
+++ A_Arrays/SubArr.java
@@ -1,6 +1,22 @@
 // subarrys is a continuous part of array.
 public class SubArr {
+    public static void sArray(int num[]){
+        int ts = 0;
+        for(int i = 0 ; i < num.length;i++){
+            int start = i ;
+            for(int j = i ; j < num.length ; j++){
+             int end = j;
+                for(int k = start ;k<= end ; k++){
+                    System.out.print(num[k]+" ");
+                }
+                System.out.println();
+                ts++;
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        
+        int num[]={2,4,6,8,10};
+        sArray(num);
     }
 }
