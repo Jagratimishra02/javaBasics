00ed563fa7eb84ade555d232cf9d20a19f70acf3
Jagratimishra02 <nishimisra002@gmail.com>
2026-06-26 17:54:40 +0000

    completed all negative cases also

--- A_Arrays/KadansAlgorithm.java
+++ A_Arrays/KadansAlgorithm.java
@@ -6,15 +6,20 @@ public class KadansAlgorithm {
         int maxsum = Integer.MIN_VALUE;
         for(int i = 0 ; i< num.length ;i++){
             currsum = currsum + num[i];
+            maxsum = Math.max(maxsum, currsum);   // it will give max sum 
             if(currsum < 0){
                 currsum = 0 ;
             }
-            maxsum = Math.max(maxsum, currsum);   // it will give max sum 
+            
         }
         System.out.println("maxsum is : " + maxsum);
     }
     public static void main(String[] args) {
         int num[] = {-2,-3,4,-1,-2,1,5,-3};
         kadanessum(num);
+
+        // covers all negative cases
+        int num2[] = { -1,-5,-6,-4,-2};
+        kadanessum(num2);
     }
 }
