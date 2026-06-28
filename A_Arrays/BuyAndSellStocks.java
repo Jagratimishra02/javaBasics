e67d132bad9444e07d24cf09e98ceadc5cea0800
Jagratimishra02 <nishimisra002@gmail.com>
2026-06-28 13:00:41 +0000

    buy and sell stocks question completed

--- A_Arrays/BuyAndSellStocks.java
+++ A_Arrays/BuyAndSellStocks.java
@@ -1,3 +1,29 @@
 public class BuyAndSellStocks {
-    
+    public static int buyAndsellstocks(int prices[]){
+        int buyPrice = Integer.MAX_VALUE ;
+        int maxprofit = 0;
+        int bestbuy = 0;
+        int bestsell = 0;
+        for(int i = 0 ; i < prices.length ;i++){
+            if (buyPrice < prices [i]) {                 // profit 
+                int profit = prices[i]-buyPrice ;        // todays profit 
+                // maxprofit = Math.max(maxprofit,profit);
+                if(maxprofit < profit){
+                    maxprofit = profit;
+                    bestbuy = buyPrice;
+                    bestsell = prices[i];
+                }
+            } else {
+                buyPrice = prices[i];                // if price(selling price)< buyprice ,so update buyprice = price 
+            }
+        }
+
+        System.out.println("Buy price : " + bestbuy);
+        System.out.println("Sell price : " + bestsell);
+        return maxprofit;
+    }
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println("max profit is : " + buyAndsellstocks(prices));
+    }
 }
