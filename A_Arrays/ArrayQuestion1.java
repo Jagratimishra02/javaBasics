56a38abe5a57a58126aedb56e4dcf178edf2f7a5
Jagratimishra02 <nishimisra002@gmail.com>
2026-06-28 17:28:03 +0000

    array question 1 completed

--- A_Arrays/ArrayQuestion1.java
+++ A_Arrays/ArrayQuestion1.java
@@ -4,18 +4,23 @@ if every element is unique */
 
 public class ArrayQuestion1 {
     public static boolean repeatednum(int num[]){
+        boolean found = false;
         for(int i = 0 ; i < num.length-1 ; i++){                // for i = 0,1,2  
             for( int j = i + 1 ; j < num.length ; j++){           // for j = 1,2,3
                 if (num[i] == num[j]) {
-                    System.out.println("number repeated is " + num[i]);
-                    return true ;
+                    // System.out.print("number repeated is " + num[i]);
+                    found = true;
+                    // return true ;
+                    break ;
                 }
+
             }
         }
-        return false ;
+        return found ;
     }
     public static void main(String[] args) {
-        int num[] = {1,2,3,1};
-        repeatednum(num);
+        // int num[] = {1,2,3,1};
+        int num[] = {1,1,1,3,3,4,3,2,4,2};
+        System.out.println(repeatednum(num));
     }
 }
