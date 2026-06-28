096de5084655131fcadb49ea5e3e63ff779ccfd2
Jagratimishra02 <nishimisra002@gmail.com>
2026-06-28 13:16:28 +0000

    array question 1

--- A_Arrays/ArrayQuestion1.java
+++ A_Arrays/ArrayQuestion1.java
@@ -1,3 +1,10 @@
 public class ArrayQuestion1 {
-    
+    public static void repeatednum(int num[]){
+        for(int i = 0 ; i < num.length ; i++){
+            for( int j = i + 1 ; j )
+        }
+    }
+    public static void main(String[] args) {
+        int num[] = {1,2,3,1};
+    }
 }
