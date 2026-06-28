2b3ed7c570cf2f9c0711447b333716e6ce07a576
Jagratimishra02 <nishimisra002@gmail.com>
2026-06-28 13:09:48 +0000

    question added

--- A_Arrays/BuyAndSellStocks.java
+++ A_Arrays/BuyAndSellStocks.java
@@ -1,3 +1,15 @@
+/* Buy & Sell Stocks
+Question:
+You are given an array prices where prices[i] is the price of a given stock on the i-th day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future
+ to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+Example:
+prices = [7, 1, 5, 3, 6, 4]
+Formula:
+profit = Sell - Buy
+(Buy low ↓, Sell high ↑) */
+
 public class BuyAndSellStocks {
     public static int buyAndsellstocks(int prices[]){
         int buyPrice = Integer.MAX_VALUE ;
