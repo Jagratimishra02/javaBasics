542bc842fc4158a6fef8b950e32920fd1e0edcf5
Jagratimishra02 <nishimisra002@gmail.com>
2026-06-28 16:58:23 +0000

    to check repeated element in array

--- A_Arrays/ArrayQuestion1.java
+++ A_Arrays/ArrayQuestion1.java
@@ -1,10 +1,18 @@
+// brute force - time complexity = O(n^2)
 public class ArrayQuestion1 {
-    public static void repeatednum(int num[]){
-        for(int i = 0 ; i < num.length ; i++){
-            for( int j = i + 1 ; j )
+    public static boolean repeatednum(int num[]){
+        for(int i = 0 ; i < num.length-1 ; i++){                // for i = 0,1,2  
+            for( int j = i + 1 ; j < num.length ; j++){           // for j = 1,2,3
+                if (num[i] == num[j]) {
+                    System.out.println("number repeated is " + num[i]);
+                    return true ;
+                }
+            }
         }
+        return false ;
     }
     public static void main(String[] args) {
         int num[] = {1,2,3,1};
+        repeatednum(num);
     }
 }
