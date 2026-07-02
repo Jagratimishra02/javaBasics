9b6a88b3163b772519ebba6e8c953332bfad0923
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-02 14:13:28 +0000

    selection sort started

--- Sorting_Algoritm/Selection_sort.java
+++ Sorting_Algoritm/Selection_sort.java
@@ -1,7 +1,22 @@
 // to select smallest and sort the array
 
 public class Selection_sort {
+    public static void selectionsort(int arr []){
+        int n = arr.length;
+        for(int i = 0 ; i < n - 1 ; i++){
+            int minpos = i ;                         // let minimum position be arr[0](which is 1)
+            for(int j = i + 1 ; j < n ; j++){
+                if(arr[minpos]>arr[j]){
+                    minpos = j ;
+                }
+            }
+            // swap 
+            int temp = arr[minpos] ;
+            arr[minpos] = arr[i];
+            arr[i] = temp;
+        }
+    }
     public static void main(String[] args) {
-        
+        int arr[] = {1,4,5,2,3};
     }
 }
