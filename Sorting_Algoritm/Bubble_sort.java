2407ecd42c213a85eb06c5eaf6d1eb1ed3c36013
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-04 16:00:48 +0000

    code optimised

--- Sorting_Algoritm/Bubble_sort.java
+++ Sorting_Algoritm/Bubble_sort.java
@@ -2,10 +2,12 @@
 // [1,2,3,4,5] - sorted array in assending order
 // [5,4,3,2,1] - sorted array in descending order
 
-// its time complexity is O(n^2) , which is not optimal
+// its time complexity is O(n^2) for worst case , which is not optimal  but after adding swap
+// method we can decrease the time complexity , for best case time complexity is O(n).
 
 public class Bubble_sort {
     public static void compare (int num[]){
+        int swap = 0 ;      // for best case it reduces time complexity
         for(int turns = 0 ; turns<num.length-1;turns++){
             for(int j = 0 ; j < num.length-1-turns; j++){
                 if(num[j] > num[j+1]){
@@ -13,8 +15,10 @@ public class Bubble_sort {
                     int temp = num[j];
                     num [j] = num[j+1];
                     num[j+1] = temp;
+                    swap++;
                 }
             }
+            if (swap == 0 ) break;           // if array is sorted
         }
     }
     public static void printnum(int num[]) {
