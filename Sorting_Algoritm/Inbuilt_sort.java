5c6f599c39c90302b17fa8c7a704046c450c567d
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-06 16:59:55 +0000

    inbuild sort completed

---
+++ Sorting_Algoritm/Inbuilt_sort.java
@@ -0,0 +1,32 @@
+// inbuilt sort in an algorithm already build in java , its time complexity is O(n).
+// package meeded - import java.util.Arrays; 
+// algorithm - Arrays.sort(arr);
+
+import java.util.Arrays;
+public class Inbuilt_sort {
+    public static void printarr(int arr[]){
+        for (int i =0 ; i < arr.length ; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = { 5,4,1,2,3};
+        // Arrays.sort(arr);
+
+        // for particular index sort
+        Arrays.sort(arr,0 ,3);       // here  0 is starting index and 3 is ending index + 1
+        printarr(arr);
+    }
+}
+
+
+// to reverse an array 
+// package used :-import java.util.Collections;
+// algorithm- Arrays.sort(arr,Collections.reverseOrder());
+// for particular index :- Arrays.sort(arr,si , ei,Collections.reverseOrder()); 
+// // si is starting index , ei is ending index + 1
+
+
+
+
