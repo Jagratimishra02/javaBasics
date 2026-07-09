35fb7823ffe3ff35bfe5c2fe64eef0b1eed20a15
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-09 11:42:52 +0000

    completed

--- 03_ConditionalStatement/IsIntegerOrNot.java
+++ 03_ConditionalStatement/IsIntegerOrNot.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class IsIntegerOrNot {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+        int x = (int)n ;
+        if(n == x){                 // or if(n-x == 0)
+            System.out.println(n + " is an integer");
+        } else {
+            System.out.println(n + " is not an integer");
+        }
     }
 }
