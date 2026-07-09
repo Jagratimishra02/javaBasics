900613f3207746cd40c23f6576b74bc270f2ba0d
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-09 11:47:00 +0000

    comment added

--- 03_ConditionalStatement/IsIntegerOrNot.java
+++ 03_ConditionalStatement/IsIntegerOrNot.java
@@ -1,9 +1,12 @@
+// integer = 1,-1 ,2,3,-4 
+// not an integer = 3.2,4.5 , 8/9 (fractional and decimal number is not an integer)
+
 import java.util.*;
 public class IsIntegerOrNot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double n = sc.nextDouble();
-        int x = (int)n ;
+        double n = sc.nextDouble();                       // eg: let n = 6.5
+        int x = (int)n ;                                  // x = 6 ,  x != n or x-n != 0 ,so it is not an integer
         if(n == x){                 // or if(n-x == 0)
             System.out.println(n + " is an integer");
         } else {
