bd5e8ec92e6160218e339855a00c67884cf68e16
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-10 18:15:31 +0000

    code modified

--- Sorting_Algoritm/Counting_Sort.java
+++ Sorting_Algoritm/Counting_Sort.java
@@ -2,14 +2,23 @@
 
 public class Counting_Sort {
     public static void countingsort(int arr[]){ 
+        
+        // to get largest 
+
         int largest = Integer.MIN_VALUE;
-        for(int i = 0 ; i < arr.length ; i++){
+        for(int i = 0 ; i < arr.length ; i++){           
           largest =  Math.max(largest, arr[i]);
         }
-        int count[] = new int[largest+1];
+
+        // to create a array that calculates frequency of element 
+
+        int count[] = new int[largest+1];  // length + 1 , bcoz index start from 0 , here largest is 7 , so length is 8
         for(int j = 0 ; j < arr.length ; j++ ){
-            count[arr[j]]++;
+            count[arr[j]]++;  
         }
+
+        // to create a sorted array with handling repeatation
+
         int k = 0;
         for(int i = 0 ; i < count.length ; i++){
             while(count[i] > 0){
