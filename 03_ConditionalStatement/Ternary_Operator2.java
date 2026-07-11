0eb213d77c37dd4c8b97e8805f287ac8a7d56bb8
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-11 16:14:28 +0000

    nested ternary operator completed

---
+++ 03_ConditionalStatement/Ternary_Operator2.java
@@ -0,0 +1,16 @@
+//  (condition ? true : false )
+
+import java.util.*;
+public class Ternary_Operator2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your first number : ");
+        int a = sc.nextInt();
+        System.out.println("enter your second number : ");
+        int b = sc.nextInt();
+        System.out.println("enter your third number : ");
+        int c = sc.nextInt();
+        // nested ternary operator
+        System.out.println("max is " + ((a >= b) ? ((a >= c)? a : c ): ((b >= c) ? b : c)));
+    }
+}
