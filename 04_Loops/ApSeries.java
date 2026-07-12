e0da3d318bc3474a61a0bb4595ebc5811b366344
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-12 13:32:20 +0000

    method 2 added

--- 04_Loops/ApSeries.java
+++ 04_Loops/ApSeries.java
@@ -6,8 +6,16 @@ public class ApSeries {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();        // here n = number of terms
-        for(int i =2 ; i <= 3*n -1 ; i += 3){     // last term = a + (n-1)d , so it is 2 + (n-1)3 = 3*n-1
-            System.out.print(i + " ");
+        // for(int i =2 ; i <= 3*n -1 ; i += 3){     // last term = a + (n-1)d , so it is 2 + (n-1)3 = 3*n-1
+        //     System.out.print(i + " ");
+        // }
+
+        // ++++++++++++++++ Method 2 ++++++++++++++++++++++++ better
+        int a = 4;
+        int d = 6 ;
+        for (int i = 1 ;i <= n ; i++){
+            System.out.print(a + " ");
+            a += d ;
         }
     }
 }
