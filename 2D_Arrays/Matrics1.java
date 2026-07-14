e26282541376b0e3047332625122195cea69c87f
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-14 17:57:16 +0000

    key function added

--- 2D_Arrays/Matrics1.java
+++ 2D_Arrays/Matrics1.java
@@ -2,6 +2,17 @@
 import java.util.Scanner;
 
 public class Matrics1 {
+    public static boolean findkey(int matrix[][] , int key){
+        for(int i = 0 ; i < matrix.length ;i++){
+            for (int j = 0 ; j < matrix[0].length; j++){
+                if(matrix[i][j] == key){
+                    System.out.println("key at cell " + "(" + i + "," +  j + ")");
+                    return true;
+                }
+            }
+        }
+        return false ;
+    }
     public static void main(String[] args) {
         int matrix [][] = new int [3][3];
         Scanner sc = new Scanner (System.in);
@@ -21,5 +32,6 @@ public class Matrics1 {
             }
             System.out.println();
         }
+        findkey(matrix,5);
     }
 }
