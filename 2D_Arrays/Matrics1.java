093257152521cfec693e4e77c0472f7a21e4714b
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-14 18:24:48 +0000

    maximum completed

--- 2D_Arrays/Matrics1.java
+++ 2D_Arrays/Matrics1.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Matrics1 {
     public static void max(int matrix[][]){
+        // To calculate maximun
          int max = Integer.MIN_VALUE;  // to calculate maximum of the matrix
         for(int i = 0 ; i < matrix.length ;i++){
             int max2 = Integer.MIN_VALUE;   // to calculate max for every row
@@ -16,6 +17,20 @@ public class Matrics1 {
         }
         System.out.println("maxumum of array is " + max);
     }
+    // public static void max(int matrix[][]){
+    //     // To calculate maximun
+    //      int max = Integer.MIN_VALUE;  // to calculate maximum of the matrix
+    //     for(int i = 0 ; i < matrix.length ;i++){
+    //         int max2 = Integer.MIN_VALUE;   // to calculate max for every row
+    //         for (int j = 0 ; j < matrix[0].length; j++){
+    //             if(matrix[i][j] > max ){
+    //                 max = matrix[i][j];
+    //             }
+    //         }
+    //         // System.out.println(max2);
+    //     }
+    //     System.out.println("maxumum of array is " + max);
+    // }
     // To find key 
     public static boolean findkey(int matrix[][] , int key){
         for(int i = 0 ; i < matrix.length ;i++){
