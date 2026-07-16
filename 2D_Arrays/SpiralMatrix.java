36493b082b5d7627146be03d34ee2de629f944c8
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-16 18:23:48 +0000

    rors column created

--- 2D_Arrays/SpiralMatrix.java
+++ 2D_Arrays/SpiralMatrix.java
@@ -1,8 +1,12 @@
 // To print an array in spiral form
 
 public class SpiralMatrix{
-    public static void printSpiral(int matrix){
-        
+    public static void printSpiral(int matrix[][]){
+        int StartRow = 0 ;
+        int StartColumn = 0 ;
+        int endRow = matrix.length - 1;
+        int endColumn =matrix[0].length - 1 ; 
+
     }
     public static void main(String[] args) {
         int matrix[][] = {{1,2,3,4,} ,
