0b2302e14f74f88f3184ea8d9c080a7554beafc5
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-18 15:08:11 +0000

    alternatePattern completed

---
+++ 04_Loops/AlternatePattern.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class AlternatePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        int low = 1;
+        int high = n;
+
+        for (int i = 0; low <= high; i++) {
+            if (i % 2 == 0) {
+                System.out.println(high);
+                high--;
+            } else {
+                System.out.println(low);
+                low++;
+            }
+        }
+
+        sc.close();
+    }
+}
