67b57b8f6f289e845ebd19e92773e305ce14708d
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-18 15:29:42 +0000

    Ascii value question completed

---
+++ 04_Loops/Ascii_Nums.java
@@ -0,0 +1,9 @@
+
+public class Ascii_Nums {
+    public static void main(String[] args) {
+        for(int i = 65 ; i <= 90 ; i++ ){  // 65 - A , 
+            System.out.print(i + " = ");    // print integer
+            System.out.println((char)i);    // print same integers ascii value 
+        }
+    }
+}
