f087f76fdb0d127e221bdbd06e0210eedc38dc26
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-19 05:50:57 +0000

    comment added

--- A_Arrays/Deep_CopyofArr.java
+++ A_Arrays/Deep_CopyofArr.java
@@ -1,10 +1,13 @@
+// deep copies means to create copy , for example ;- PRINTOUT Of a x paper is y (deep copy)
+// shallow copy :- change the orignal x paper to name paper
+
 import java.util.Arrays;
 
 public class Deep_CopyofArr {
     public static void main(String[] args) {
          int []arr = { 1, 4 , 6, 7, 8 };
-         int []x = Arrays.copyOf(arr, arr.length); 
-         x[0] = 9;
+         int []x = Arrays.copyOf(arr, arr.length);  // deep copy created 
+         x[0] = 9;                                  // change in deep copy 
          System.out.println(arr[0]);
          System.out.println(x[0]);
     }
