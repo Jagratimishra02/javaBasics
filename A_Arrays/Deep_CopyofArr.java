8aeff36fc247ced3ad210c95922f87349abb2be1
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-19 06:46:32 +0000

    shallow copy

--- A_Arrays/Deep_CopyofArr.java
+++ A_Arrays/Deep_CopyofArr.java
@@ -6,9 +6,14 @@ import java.util.Arrays;
 public class Deep_CopyofArr {
     public static void main(String[] args) {
          int []arr = { 1, 4 , 6, 7, 8 };
-         int []x = Arrays.copyOf(arr, arr.length);  // deep copy created 
-         x[0] = 9;                                  // change in deep copy 
-         System.out.println(arr[0]);
+         // shaLLOW COPY
+         int []x = arr;
+         x[0] = 3 ;       // shallow copy   , this changes orignal array so, x[0] = 3 and so on arr[0] = 3.
+
+        //  int []x = Arrays.copyOf(arr, arr.length);  // deep copy created 
+        //  x[0] = 9;                                  // change in deep copy 
+       
+        System.out.println(arr[0]);
          System.out.println(x[0]);
     }
 }
