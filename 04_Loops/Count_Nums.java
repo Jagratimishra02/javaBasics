2a773a19101de5122aba1363e560942986e4562f
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-22 16:53:48 +0000

    comment added

--- 04_Loops/Count_Nums.java
+++ 04_Loops/Count_Nums.java
@@ -4,9 +4,10 @@ public class Count_Nums {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         int n = sc.nextInt();
+        // to hadle 0 input case 
         int count = 0 ;
         if ( n == 0){
-        n = 2;
+        n = 2;          // replace 0 with any one digit number 
         }
         while( n != 0){
             n /= 10;
