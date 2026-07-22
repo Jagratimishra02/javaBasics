9c09b14d1f9eded8ffe408112957ca1fcc5d12ed
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-22 16:52:38 +0000

    count loop started

--- 04_Loops/Count_Nums.java
+++ 04_Loops/Count_Nums.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Count_Nums {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        int count = 0 ;
+        if ( n == 0){
+        n = 2;
+        }
+        while( n != 0){
+            n /= 10;
+            count ++;
+        }
+        System.out.println(count);
     }
 }
