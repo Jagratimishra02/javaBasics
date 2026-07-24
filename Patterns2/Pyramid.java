01127c2e83655f0ef544a2c828f799757be04948
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-24 14:33:02 +0000

    method two

--- Patterns2/Pyramid.java
+++ Patterns2/Pyramid.java
@@ -5,15 +5,33 @@ public class Pyramid {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        // for(int i = 1 ; i <= n ; i++){
+        //     // for spaces 
+        //     for(int j = 1 ; j <= n- i ; j++){
+        //         System.out.print("  ");
+        //     }
+        //     // for stars
+        //     for(int k = 1 ; k <= 2*i-1 ; k++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+//++++++++++++ Method 2 ++++++++++++++++++++++++++++
+
+         int spaces = n-1 ;
+         int stars = 1;
         for(int i = 1 ; i <= n ; i++){
             // for spaces 
-            for(int j = 1 ; j <= n- i ; j++){
-                System.out.print("  ");
+            for(int j = 1 ; j <= spaces ; j++){
+                System.out.print(" " + " ");
             }
-            // for stars
-            for(int k = 1 ; k <= 2*i-1 ; k++){
-                System.out.print("* ");
+            // for stars 
+            for(int k = 1 ; k <= stars ; k++){
+                System.out.print("*" + " ");
             }
+            stars += 2;
+            spaces--;
             System.out.println();
         }
         
