ffed021ca930607ec1aa0afcc5e90e965a721e43
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-24 13:47:44 +0000

    code completed

--- Patterns2/Pyramid.java
+++ Patterns2/Pyramid.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Pyramid {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1 ; i <= n ; i++){
+            // for spaces 
+            for(int j = 1 ; j <= n- i ; j++){
+                System.out.print("  ");
+            }
+            // for stars
+            for(int k = 1 ; k <= 2*i-1 ; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
         
     }
 }
