94e99fdf35ce111bfaeb45818841441887e2b27f
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-25 13:59:12 +0000

    code modified

--- FunctionsAndMethods/Return.java
+++ FunctionsAndMethods/Return.java
@@ -2,11 +2,13 @@
 
 public class Return {
     public static int return_check(int n){
-        if( n > 0) return n ;
+        if( n > 0){
+            return n ;
+        } 
         // else is imp bcoz what if a first statement is false then what is returned in int 
         // ,so else is imp if function returns something .
-        
-        else return -1 ;            // we need to return anything in else also if uses "if " . it is mandatory
+
+        return -1 ;            // we need to return anything in else also if uses "if " . it is mandatory
     }
 
     public static void main(String[] args) {
