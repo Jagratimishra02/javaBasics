dd6d74fa64131bb5039c6f508810e97fb38301e5
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-25 13:39:18 +0000

    maximum of three completed

--- FunctionsAndMethods/Maxof3Nums.java
+++ FunctionsAndMethods/Maxof3Nums.java
@@ -1,5 +1,14 @@
 public class Maxof3Nums {
+    public static int max(int a , int b , int c){
+        if(a > b && a > c){
+            return a;
+        } else if(b > a && b > c){
+            return b ;
+        } else {
+            return c;
+        }
+    }
     public static void main(String[] args) {
-        
+      System.out.println(max( 4, 5 ,6));  
     }
 }
