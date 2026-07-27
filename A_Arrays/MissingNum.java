3b9f1e07d393e9a1e0a397f5ed454e718411c1ea
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-27 05:21:05 +0000

    comment added

--- A_Arrays/MissingNum.java
+++ A_Arrays/MissingNum.java
@@ -1,14 +1,21 @@
+// To find missing element from array 
+// Eg : {1, 2 , 4 ,5} ,Array length (n) = 4   => missing element is 3 
+// orignal array without missing element should be => { 1, 2 ,3 ,4, 5 } here array length(n) becomes n+ 1 = 5
+
 import java.util.Scanner;
 
 public class MissingNum {
     public static void missingnum(int Arr[]){
         int n = Arr.length+1;
+       // sum of orignal array which does not have missing element 
         int sum = n *(n+1)/2 ;
+
+      // sum of array having missing num.
         int summissing = 0 ;
         for(int i : Arr){
             summissing += i;
         }
-        System.out.println(sum-summissing);
+        System.out.println(sum-summissing);  //  orignal array sum - missing array sum
     }
     public static void printArr(int Arr[]){
         for(int val : Arr){
