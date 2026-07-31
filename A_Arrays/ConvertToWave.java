be285ec5c2448a96d7e7327eeed948fbee75c06f
Jagratimishra02 <nishimisra002@gmail.com>
2026-07-31 18:13:20 +0000

    comment added

--- A_Arrays/ConvertToWave.java
+++ A_Arrays/ConvertToWave.java
@@ -1,3 +1,5 @@
+//To convert array into wave . eg : -  int []arr = {1,2,3,4,5}; into {2,1,4,3,5}
+
 public class ConvertToWave {
     public static void convert_into_wave(int arr[]){
         // for odd num arr i + 1 will be out bound so i < arr.length -1.
@@ -8,6 +10,7 @@ public class ConvertToWave {
          arr[i+1] = temp;
         } 
     }
+    // to print an array using for each loop.
      public static void printarr(int arr[]){
         for( int value : arr){
             System.out.print(value + " " );
