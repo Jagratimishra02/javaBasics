5cf3912a6d1747e8d22dabd16c2d48e6237c79f5
Jagratimishra02 <nishimisra002@gmail.com>
2026-08-07 12:44:47 +0000

    array list started

--- ArrayList/Syntax_of_arraylist.java
+++ ArrayList/Syntax_of_arraylist.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+
 public class Syntax_of_arraylist {
      public static void main(String[] args) {
-        
+       ArrayList<Integer> arr = new ArrayList<> ();
+       arr.add(25);
+       arr.add(21);
+       arr.add(18);
+       arr.add(5);
+       arr.add(10);
      }
 }
