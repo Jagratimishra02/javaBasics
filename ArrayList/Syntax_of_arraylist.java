ee2cb9bf9dccae51fb9a2c4860867ff230701643
Jagratimishra02 <nishimisra002@gmail.com>
2026-08-07 12:50:43 +0000

    one more comment added

--- ArrayList/Syntax_of_arraylist.java
+++ ArrayList/Syntax_of_arraylist.java
@@ -9,5 +9,7 @@ public class Syntax_of_arraylist {
        arr.add(5);
        arr.add(10);
        System.out.println(arr.get(2));       // for array we use arr[2] , but in array list we use arr.get(2).
+       arr.set(2, 22);
+       System.out.println(arr.get(2));   // this is analogous to arr[2] = 22 , in array . 
      }
 }
