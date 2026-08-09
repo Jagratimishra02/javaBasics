b50564d422e6768ed0352d4e6160c6dd597cf9f7
Jagratimishra02 <nishimisra002@gmail.com>
2026-08-09 07:50:41 +0000

    array list modified

--- ArrayList/Syntax_of_arraylist.java
+++ ArrayList/Syntax_of_arraylist.java
@@ -10,6 +10,10 @@ public class Syntax_of_arraylist {
        arr.add(10);
        System.out.println(arr.get(2));       // for array we use arr[2] , but in array list we use arr.get(2).
        arr.set(2, 22);
-       System.out.println(arr.get(2));   // this is analogous to arr[2] = 22 , in array . 
+       System.out.println(arr.get(2));   // this is analogous to arr[2] = 22 , in array .
+       
+       // to print the array list 
+       System.out.println(arr);
+
      }
 }
