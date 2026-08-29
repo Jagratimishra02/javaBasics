509ac50b2630fb2706d7e9b69bf1ab0c8e8c34f0
Jagratimishra02 <nishimisra002@gmail.com>
2026-08-29 15:30:30 +0000

    minimum of maximum of rows

---
+++ 2D_Arrays/MaximumArr.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class MaximumArr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int arr [][] = new int [n][m];
+        int min = Integer.MAX_VALUE;
+        for(int i = 0 ; i < n ; i++){
+            int max = Integer.MIN_VALUE;
+            for(int j = 0 ; j < m ; j++){
+                 arr[i][j] = sc.nextInt();
+                System.out.print(arr[i][j]+" ");
+                if(max<arr[i][j]){
+                    max = arr[i][j];
+                }
+            }
+            if(max<min){
+                min = max ;
+            }
+            System.out.println();
+        }
+        System.out.println(min);
+    }
+}
