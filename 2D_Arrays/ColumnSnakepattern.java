eb8996f827869dbd23901668037dc7b87577c169
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-01 16:06:10 +0000

    to snake pattern columnwise

---
+++ 2D_Arrays/ColumnSnakepattern.java
@@ -0,0 +1,20 @@
+public class ColumnSnakepattern {
+    public static void printsnake(int arr[][]){
+        for(int i = 0 ; i < arr.length ;i++ ){
+            if(i % 2 == 0) {
+                for(int j = 0 ; j < arr[i].length ; j++){
+                    System.out.print(arr[i][j] + " ");
+                } 
+              } else { for(int j = arr[i].length-1; j >= 0 ; j-- ){
+                    System.out.print(arr[i][j] + " ");
+               
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int [][] arr = {{1,2,3,4},{5,6,7,8},{2,4,6,8},{3,5,7,9}};
+        printsnake( arr);
+    }
+}
