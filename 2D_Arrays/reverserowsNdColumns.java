5a4ca4d8dc72cc6f6aa5830aca79f8144f74c277
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-02 12:48:13 +0000

    code modified

--- 2D_Arrays/reverserowsNdColumns.java
+++ 2D_Arrays/reverserowsNdColumns.java
@@ -5,7 +5,7 @@ public class reverserowsNdColumns {
         for(int i = arr.length-1 ; i >= 0 ;i--){
             // to traverse inner array .  
              for(int j = arr[0].length-1 ; j >= 0 ; j-- ){
-             System.out.print(arr[i][j]);
+             System.out.print(arr[i][j] + " ");
           }
           System.out.println();
         }
