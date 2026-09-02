7451f0610a9899d328c6b3dcadc49e35487eaac2
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-02 05:28:41 +0000

    transpose completed

---
+++ 2D_Arrays/Transposematrix.java
@@ -0,0 +1,18 @@
+public class Transposematrix {
+   public static void printsnake(int arr[][]){
+     for(int i = 0 ; i < arr[0].length;i++){
+        // rows reverse
+        for(int j = 0 ; j < arr.length;j++){
+            System.out.print(arr[j][i]);
+        }
+        System.out.println();
+        }
+       
+      }
+  
+    public static void main(String[] args) {
+         int [][] arr = {{1,2,3,4},{5,6,7,8},{2,4,6,8},{3,5,7,9},{9,7,6,3}};
+        printsnake( arr);
+    }
+}
+
