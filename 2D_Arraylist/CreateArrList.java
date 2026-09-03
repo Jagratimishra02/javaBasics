4fa4dac3d2fb7acc575c2f3ab6dd27a90343e2fb
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-03 16:49:39 +0000

    code modified

--- 2D_Arraylist/CreateArrList.java
+++ 2D_Arraylist/CreateArrList.java
@@ -14,10 +14,24 @@ public class CreateArrList {
         arr.add(a); 
         arr.add(b);
         arr.add(c);
+
+  //+++++++++++++++ to print array list +++++++++++
+
+  // method 1 
         // System.out.println(arr);
-        for(int i = 0 ; i < arr.size() ; i++){
-            for(int j = 0 ; j < arr.get(i).size(); j++){
-                System.out.print(arr.get(i).get(j) + " ");
+
+  // method 2
+        // for(int i = 0 ; i < arr.size() ; i++){
+        //     for(int j = 0 ; j < arr.get(i).size(); j++){
+        //         System.out.print(arr.get(i).get(j) + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // method 3 
+        for(ArrayList<Integer> list : arr){
+            for(int ele : list){
+                System.out.print(ele);
             }
             System.out.println();
         }
