b3fde651acca01cd6cb200756bae085bfad4b66e
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-05 05:33:59 +0000

    ArrayList printed

--- 2D_Arraylist/PascalTriangle.java
+++ 2D_Arraylist/PascalTriangle.java
@@ -2,20 +2,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PascalTriangle {
-     public List<List<Integer>> generate(int n) {
+     public static  List<List<Integer>> generate(int numRows) {
         // array list of array list created
         List<List<Integer>> ans = new ArrayList<>();
-        for(int i = 0 ; i < n ; i++){
-           //  arraylist added into empty arraylist
+         for(int i = 0 ; i < numRows ; i++){
             ans.add(new ArrayList<Integer>());
             for(int j = 0 ; j <= i ; j++){
-                if(i == 0 || i == j) ans.get(i).add(1);
-                else {
+                if(j == 0 || i == j) ans.get(i).add(1);
+                 else {
                     int val = ans.get(i-1).get(j)+ans.get(i-1).get(j-1);
                     ans.get(i).add(val);
                 }
             }
-        }
+            
+        } 
         return ans;
     }
+    public static void main(String[] args) {
+        int n = 5;
+
+        // method 1 to print array list and call function.
+        System.out.println(generate(n));
+
+        // or method 2 
+        //  List<List<Integer>> result = generate(n);
+        //  System.out.println(result);
+  }
 }
