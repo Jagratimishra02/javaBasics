4f598af87b5a1df80ba521cb87708f66bd82903b
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-08 14:10:07 +0000

    palindrome completed

---
+++ Strings/palindrome.java
@@ -0,0 +1,20 @@
+package Strings;
+import java.util.Scanner;
+
+public class palindrome {
+    public static boolean isPalindrome(String s) {
+        int start = 0 ;
+        int end = s.length()-1;
+        while(start<=end){
+            if(s.charAt(start) != s.charAt(end)) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+       public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(isPalindrome(s));
+   }  
+}
