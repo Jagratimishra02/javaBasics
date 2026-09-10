de40c0959d6b40d2e2a97a05143174bbfb127d39
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-10 13:38:37 +0000

    count digit completed

--- Strings/CountDigits.java
+++ Strings/CountDigits.java
@@ -5,6 +5,7 @@ public class CountDigits {
      Scanner sc = new Scanner(System.in);
      int n = sc.nextInt();          
      String s = "" + n;
+     // to count digits in n without using loop convert it into string then use s.length .
      System.out.println(s.length());   // if n = 000234 output is 3 , 3453 output = 4
     }
 }
