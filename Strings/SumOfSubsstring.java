1600367cd1d459bfaaef010af7858e26183c4b91
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-14 16:32:49 +0000

    started

--- Strings/SumOfSubsstring.java
+++ Strings/SumOfSubsstring.java
@@ -1,10 +1,11 @@
 public class SumOfSubsstring {
     public static int sumSubstrings(String s) {
         // code here
-       return 1; 
+     
     }
     public static void main(String[] args) {
-        String s = "6759";
-        sumSubstrings( s);
+        // String s = "6759";
+        String s = "122";
+        System.out.println(sumSubstrings( s));
     }
 }
