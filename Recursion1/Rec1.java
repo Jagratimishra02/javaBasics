47330c05ab431103e5008233a40fe36214cf9af3
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-17 12:55:05 +0000

    recursion started

--- Recursion1/Rec1.java
+++ Recursion1/Rec1.java
@@ -1,7 +1,12 @@
 // Recursion refers to function calling itself.
+
+import java.util.Scanner;
+
 public class Rec1 {
     public static void main(String[] args) {
-        print(5);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        print(n);
     }
     // to print 5 to 1
     public static void print(int n){
