dc1ffeed70eaee6775a316f1d0c2844cab1356a2
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-18 04:59:43 +0000

    global variable started

---
+++ Recursion1/GlobalVar.java
@@ -0,0 +1,11 @@
+public class GlobalVar {
+    // global variable can be used globally ;
+    static int x = 10 ;
+    public static void main(String[] args) {
+        fun();
+        System.out.println(x);         // output is 20 .
+    } 
+    public static void fun(){
+         x = 20;                 // x changed globally .
+    }
+}
