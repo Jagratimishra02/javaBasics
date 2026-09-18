077945124638ddad23dcf30c9ef3797883f44424
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-18 05:27:37 +0000

    class created new method

--- Recursion1/FunctionCalingItself.java
+++ Recursion1/FunctionCalingItself.java
@@ -1,16 +1,30 @@
 import java.util.Scanner;
 
 public class FunctionCalingItself {
+    //   public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = sc.nextInt();
+    //     print(1,n);  // we need two parameter so one we can change , nd one constant.
+    // }
+    // // to print 1 to n
+    // public static void print(int x,int n){
+    //     if(x>n) return ;
+    //     System.out.println(x);
+    //     print(x+1,n);
+    // }
+
+    // method 2 : - 
+      static int n  ;
       public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        print(1,n);  // we need two parameter so one we can change , nd one constant.
+         n = sc.nextInt();
+        print(1);  
     }
     // to print 1 to n
-    public static void print(int x,int n){
+    public static void print(int x){
         if(x>n) return ;
         System.out.println(x);
-        print(x+1,n);
+        print(x+1);
     }
 }
  
