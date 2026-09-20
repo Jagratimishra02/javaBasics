11b9df0d14938daba246e1e4a6190b22fa8afdb7
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-20 14:32:37 +0000

    a power b code completed

---
+++ Recursion1/APowB.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class APowB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.err.println("Enter the base");
+        int a = sc.nextInt();
+        System.out.println("Enter exponent");
+        int b = sc.nextInt();
+        // method 1 inbuilt method 
+        // System.out.println(a + " raised to power " + b + " is " + Math.pow(a, b));
+
+        // method 2 
+         System.out.println(a + " raised to power " + b + " is " + pow(a, b));
+    }
+    public static int pow(int a , int b){
+        //   Extra space is taken
+        // if(b == 0) return 1;
+        // return a*pow(a, (b-1));
+
+        // method 3 
+           if(b == 0) return 1;
+           int num = pow(a, b/2);
+        if(b % 2 == 0) return num*num;   // num var created bcox calling methods again and again is pathetic.
+        else return num*num*a;
+    }
+}
