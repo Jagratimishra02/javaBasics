1dbf475a4ff74213a31e13421c020dc40787c554
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-21 12:53:17 +0000

    code modified

--- Recursion1/RecSum.java
+++ Recursion1/RecSum.java
@@ -1,5 +1,5 @@
+// sum of number n . example ,n=3:sum = 1+2+3 
 import java.util.Scanner;
-
 public class RecSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in) ;
