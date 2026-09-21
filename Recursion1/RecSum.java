0dcbea17b14a3ca00c6561daf44d9b6cbb0950e5
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-21 12:51:35 +0000

    sum of consecutive numbers

---
+++ Recursion1/RecSum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class RecSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        int n = sc.nextInt();
+        System.out.println(sum(n));
+    }
+    public static int sum(int n){
+        // method 1 using maths .
+        // return n*(n+1)/2;  fastest
+
+        // method 2  recursion
+        if(n ==1) return 1;
+        // if(n ==0) return 0;
+        return n+sum(n-1);
+    }
+}
