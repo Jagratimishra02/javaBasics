3dd7b188f485861f98013202c7e49d00ba2042c2
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-22 04:17:57 +0000

    dfs introduced

--- Recursion1/Fibonacci.java
+++ Recursion1/Fibonacci.java
@@ -9,6 +9,7 @@ public class Fibonacci {
     }
     public static int series(int n){
         if(n == 0 || n ==1) return n;
+        // it is known as depth first search . dfs
         return series(n-1)+series(n-2);  // calling two functions together.
     }
 }
