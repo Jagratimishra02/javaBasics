c99a7e2cffcdf2aa6d113d0f62ef124bc71cc829
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-23 15:01:26 +0000

    unique paths method 2 completed

--- Recursion2/UniquePaths.java
+++ Recursion2/UniquePaths.java
@@ -7,9 +7,30 @@ public class UniquePaths {
     }
 
     // Method 1 
+    //  public static int uniquePaths(int m, int n) {
+    //     if(m == 1 || n == 1)return 1 ;  // if 3*1 return 1, if 1*3 return 1, if 1*1 return 1 
+    //     // bcoz there is only one way to reach the destination
+    //     return uniquePaths(m,n-1)+uniquePaths(m-1, n); // this gives tle error, bcoz we are calculating the same 
+    //     // value again and again, so we can use memoization to store the values in a 2D array and return the value 
+    //     // if it is already calculated
+         
+        
+    //     //this is the recursive call, we can reach the
+    //     //  destination from either top or left, so we add both the ways
+    // }
+
+    // Method 2
      public static int uniquePaths(int m, int n) {
-        if(m == 1 || n == 1)return 1 ;  // if 3*1 return 1, if 1*3 return 1, if 1*1 return 1 
-        // bcoz there is only one way to reach the destination
-        return uniquePaths(m,n-1)+uniquePaths(m-1, n);
+        return Paths(0,0,m-1, n-1);
+    }
+     public static int Paths(int cr , int cc,int lr, int lc) {
+        if(cr == lr && cc == lc) return 1; // if we reach the destination, return 1
+        if(cr > lr || cc > lc) return 0; // if we go out of bounds, return 0 
+        return Paths(cr+1, cc, lr, lc) + Paths(cr, cc+1, lr, lc); // we can move either down or right
+        // int right = Paths(cr+1, cc, lr, lc);
+        // int down = Paths(cr, cc+1, lr, lc); // we can move either down or right
+        // return right + down;    // this also gives tle error, bcoz we are calculating the 
+        // same value again and again, so we can use memoization to store the values in a 2D array and 
+        // return the value if it is already calculated
     }
 }
