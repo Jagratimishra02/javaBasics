e5271f11532d21ea46a055e14a46078d1e0119c8
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-23 14:08:09 +0000

    code modified

--- Recursion2/UniquePaths.java
+++ Recursion2/UniquePaths.java
@@ -1,3 +1,4 @@
+// number of unique paths from top left to bottom right in a m x n grid 
 public class UniquePaths {
     public static void main(String[] args) {
        int m = 4;
@@ -7,7 +8,8 @@ public class UniquePaths {
 
     // Method 1 
      public static int uniquePaths(int m, int n) {
-        if(m == 1 || n == 1)return 1 ;
+        if(m == 1 || n == 1)return 1 ;  // if 3*1 return 1, if 1*3 return 1, if 1*1 return 1 
+        // bcoz there is only one way to reach the destination
         return uniquePaths(m,n-1)+uniquePaths(m-1, n);
     }
 }
