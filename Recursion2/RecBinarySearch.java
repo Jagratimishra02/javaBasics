381c6e520671d3c32e63804b55a031fc31794548
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-25 13:34:49 +0000

    code modified

--- Recursion2/RecBinarySearch.java
+++ Recursion2/RecBinarySearch.java
@@ -1,13 +1,16 @@
+// binary search using recursion , leetcode 704 .
 public class RecBinarySearch {
      public static void main(String[] args) {
         int[] nums = {-1,0,3,5,9,12};
         System.out.println(search(nums, 9));
         System.out.println(search(nums, 6));
      }
+     // Search function.
     public static int search(int[] nums, int target) {
         int n = nums.length;
         return helper(nums,target,0,n-1);
     }
+    // helper function 
     public static int helper(int[] nums, int target , int lo , int hi){
         if(hi<lo) return -1;
         int mid = lo + (hi-lo)/2 ;
