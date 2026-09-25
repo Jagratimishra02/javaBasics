1270436cb9bc4d323424dd473439166419d1b63d
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-25 13:45:55 +0000

    recursion on arrays modified

--- Recursion2/RecursionOnArrays.java
+++ Recursion2/RecursionOnArrays.java
@@ -2,7 +2,7 @@
 public class RecursionOnArrays {
     public static void main(String[] args) {
         int[] arr = {2,4,6,8,9};
-        // int n = arr.length-1; // length of the array
+        int n = arr.length-1; // length of the array
 
         recArr(arr,0); // to print array using Recursion first to last.
         // recArr(arr,n); // to print array using Recursion last to first.
@@ -14,10 +14,9 @@ public class RecursionOnArrays {
     public static void recArr(int []arr , int idx){  
         int n = arr.length; // length of the array
         if(idx == n) return;
-        System.out.println(arr[idx] + " "); // print the element at the current index
+        System.out.print(arr[idx] + " "); // print the element at the current index
         recArr(arr, idx+1);
     }
-
     // // to print array using Recursion last to first.
     // public static void recArr(int []arr , int n){  
     //     if(n == 0) {
@@ -31,9 +30,9 @@ public class RecursionOnArrays {
     // method to print array using Recursion last to first.
      public static void recReversearr(int []arr , int idx){  
         int n = arr.length; // length of the array
-        if(idx < 0) return;
-        System.out.println(arr[idx] + " ");  
-        recReversearr(arr, idx-1); 
-        // call first and then print the element at the current index printing in reverse order
+        if(idx >= n) return;
+        recReversearr(arr, idx+1); 
+        System.out.print(arr[idx] + " "); 
+    //     call first and then print the element at the current index printing in reverse order
     }
 }
