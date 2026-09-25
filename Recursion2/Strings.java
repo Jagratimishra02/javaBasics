3d551902fd3cc16fd147d8d79a6abda527b9bd65
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-25 15:19:43 +0000

    strings recursion started

---
+++ Recursion2/Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args) {
+        String s = "nishi";
+        change(s);
+        System.out.println(s);
+    }
+    public static void change(String s){
+        s = "krapansh";
+        System.out.println();
+    }
+}
