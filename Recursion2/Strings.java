cdd5cb1ad9e134a96b6b7ee697e267cc88e73065
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-26 09:21:59 +0000

    string started

--- Recursion2/Strings.java
+++ Recursion2/Strings.java
@@ -1,11 +1,32 @@
+import java.util.ArrayList;
+
 public class Strings {
     public static void main(String[] args) {
         String s = "nishi";
         change(s);
-        System.out.println(s);
+        String []arr = {"jago" , "jagrati" , "anshu" , "carino"};
+        // every element of array can be printed
+        // for(int i = 0 ; i < arr.length ; i++){
+        //  System.out.print(arr[i] + " ");
+        // }
+
+        ArrayList<String> al = new ArrayList<>();
+        al.add("kuchupuchu");
+        al.add("rasmalai");
+        al.add("hariom");
+        al.add("dosa");
+        al.add("maggie");
+        al.add("pizza");
+        System.out.println(al);
+        chngArrList(al);
+        System.out.println(al);
     }
     public static void change(String s){
         s = "krapansh";
         System.out.println();
+    }
+
+    public static void chngArrList(ArrayList<String> al){
+       al.add("garlic bread");
     }
 }
