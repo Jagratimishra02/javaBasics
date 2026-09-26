9e0c0a706bb86db74b543de708b729387a1c631e
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-26 16:06:22 +0000

    powerset completed

--- Recursion2/Powerset.java
+++ Recursion2/Powerset.java
@@ -1,15 +1,20 @@
+import java.util.*;
+
 public class Powerset {
     public static void main(String[] args) {
-     String s = "jagrati";
-     subsets("" , s, 0);
+     List<String> list = new ArrayList<>();   // if we want to return list 
+     String s = "abc";
+     subsets("" , s, 0 , list);
+      Collections.sort(list);
+     System.out.println(list);
     }
-    public static void subsets(String ans , String s , int idx){
+    public static void subsets(String ans , String s , int idx , List<String> list){
         if(idx == s.length()){
-            System.out.println(ans + " ");
+            if (ans.length()!= 0) list.add(ans);
             return;
         }
-        int ch = s.charAt(idx);
-        subsets(ans+ch , s , idx+1);
-        subsets(ans , s , idx+1);
+        char ch = s.charAt(idx);
+        subsets(ans+ch , s , idx+1,list);   // pick
+        subsets(ans , s , idx+1,list);      // skip
     }
 }
