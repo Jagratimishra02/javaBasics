b279bfde2678d873412dac0669416a7b531465ce
Jagratimishra02 <nishimisra002@gmail.com>
2026-09-26 16:17:51 +0000

    code modified

--- Recursion2/RecBinarySearch.java
+++ Recursion2/RecBinarySearch.java
@@ -1,4 +1,5 @@
 // binary search using recursion , leetcode 704 .
+// time complexity = O(n), space complexity = O(n);
 public class RecBinarySearch {
      public static void main(String[] args) {
         int[] nums = {-1,0,3,5,9,12};
